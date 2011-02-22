1f6d8c6cddd2fccfdb96e8f679ea833a3a7edf61
Masamoto Miyata <miyata@sincere-co.com>
2011-02-22 17:38:11 +0000

    git-svn-id: https://scrupload.googlecode.com/svn/branches/dev@3 6a939483-aa53-8207-cb86-4a7756a7ee3a

---
+++ gears/jquery.ui.scruploadHtml4.js
@@ -0,0 +1,44 @@
+(function($){
+$.widget('ui.scruploadHtml4', {
+	options: {
+		
+	},
+	_create: function()
+	{
+		var self = this;
+		var img = $('<img src="'+self.options.button_image+'">');
+		img.appendTo(self.element);
+		
+		var form = $('<form action="" method="post" enctype="multipart/form-data" />');
+		form.appendTo(self.element)
+			.css("overflow", "hidden")
+			.css("position", "absolute")
+			.width(img.width())
+			.height(img.height())
+			.offset(img.offset())
+			;
+		
+		var input = $('<input type="file" name="file" />');
+		input.appendTo(form)
+			.attr("size", 1)
+			.css("font-size", img.height())
+			.width("100%")
+			.height("100%")
+			.css("filter", "alpha(opacity=0)")
+			.css("-moz-opacity", "0")
+			.css("opacity", "0")
+			;
+		
+		img.click(function(event){
+			input.click();
+		});
+		
+	},
+	destroy: function()
+	{
+		$.Widget.prototype.destroy.apply(self, arguments);
+		return self;
+	}
+});
+
+})(jQuery);
