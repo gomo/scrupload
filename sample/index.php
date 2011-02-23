e8756a570013f453c9e10e2df911bdfba1dd8004
Masamoto Miyata <miyata@sincere-co.com>
2011-02-23 16:48:07 +0000

    git-svn-id: https://scrupload.googlecode.com/svn/branches/dev@10 6a939483-aa53-8207-cb86-4a7756a7ee3a

--- sample/index.php
+++ sample/index.php
@@ -9,7 +9,8 @@
 	<script type="text/javascript">
 		$(function(){
 			$("#upload_button").scruploadHtml4({
-				button_image: '/img/upload.gif'
+				html4_use_input: true
+				
 			});
 		});
 	</script>
@@ -17,5 +18,7 @@
 <body>
 <p>Scrupload demo.</p>
 <div id="upload_button"><img src="/img/upload.gif" /></div>
+<a href="javascript:void(0)" onclick="$('#upload_button').scruploadHtml4('destroy')">destroy</a>
+
 </body>
 </html>
