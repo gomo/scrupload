a5e81a36eca1ed6cf29bd38a5ef50d787cff281a
Masamoto Miyata <miyata@sincere-co.com>
2011-02-23 13:51:56 +0000

    子要素を取るようにしたら治ったので機能は残します
    git-svn-id: https://scrupload.googlecode.com/svn/branches/dev@8 6a939483-aa53-8207-cb86-4a7756a7ee3a

--- gears/jquery.ui.scruploadHtml4.js
+++ gears/jquery.ui.scruploadHtml4.js
@@ -6,46 +6,39 @@ $.widget('ui.scruploadHtml4', {
 	_create: function()
 	{
 		var self = this;
+		var button = self.element.find(':first-child');
 		
 		//chromeが画像のサイズを取得できなかった。
 		$(window).bind('load', function() {
 			self._form = $('<form action="" method="post" enctype="multipart/form-data" />');
-			var os = self.element.offset();
-			console.info(os);
 			self._form.appendTo("body")
 				.css("overflow", "hidden")
-				.css("cursor", "pointer")
 				//.css("position", "absolute")
-				.width(self.element.width())
-				.height(self.element.height())
-				.offset({top: os.top, left: os.left})
+				.css("cursor", "pointer")
+				.width(button.width())
+				.height(button.height())
+				.offset(button.offset())
 				;
 			
 			var input = $('<input type="file" name="file" />');
 			input.appendTo(self._form)
 				.attr("size", 1)
-				.css("font-size", self.element.height())
+				.css("font-size", button.height())
 				.css("cursor", "pointer")
 				.width("100%")
 				.height("100%")
 				//.css("filter", "alpha(opacity=0)")
 				//.css("-moz-opacity", "0")
 				//.css("opacity", "0")
-				.change(function(e){
-					console.info(this.value);
-				})
 				;
-			
 			var default_os = input.offset();
 			var offset_top = input.height() / 2;
 			var offset_left = (input.width() / 4) * 3;
-			self._form.mousemove(function(e){
-				input.offset({top: e.clientY - offset_top, left: e.clientX - offset_left});
+			self._form.mousemove(function(event){
+				input.offset({top: event.clientY - offset_top, left: event.clientX - offset_left});
 			}).mouseout(function(){
-				input.offset({top: default_os.top, left: default_os.left});
+				input.offset(default_os);
 			});
-			
-			self._trigger('onInit', null, {button: self.element});
 		});
 	},
 	destroy: function()
