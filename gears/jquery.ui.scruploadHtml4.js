c2bb25a49f4764699000aa5b38ded4d12ca51b31
Masamoto Miyata <miyata@sincere-co.com>
2011-02-23 13:45:34 +0000

    画像のボタンはブラウザ間で微妙な問題があるので却下
    git-svn-id: https://scrupload.googlecode.com/svn/branches/dev@7 6a939483-aa53-8207-cb86-4a7756a7ee3a

--- gears/jquery.ui.scruploadHtml4.js
+++ gears/jquery.ui.scruploadHtml4.js
@@ -6,45 +6,53 @@ $.widget('ui.scruploadHtml4', {
 	_create: function()
 	{
 		var self = this;
-		var button = self.element.find(':first-child');
-		button.css("cursor", "pointer");
 		
+		//chromeが画像のサイズを取得できなかった。
 		$(window).bind('load', function() {
-			var form = $('<form action="" method="post" enctype="multipart/form-data" />');
-			form.appendTo("body")
+			self._form = $('<form action="" method="post" enctype="multipart/form-data" />');
+			var os = self.element.offset();
+			console.info(os);
+			self._form.appendTo("body")
 				.css("overflow", "hidden")
-				.css("position", "absolute")
 				.css("cursor", "pointer")
-				.width(button.width())
-				.height(button.height())
-				.offset(button.offset())
+				//.css("position", "absolute")
+				.width(self.element.width())
+				.height(self.element.height())
+				.offset({top: os.top, left: os.left})
 				;
 			
 			var input = $('<input type="file" name="file" />');
-			input.appendTo(form)
+			input.appendTo(self._form)
 				.attr("size", 1)
-				.css("font-size", button.height())
+				.css("font-size", self.element.height())
 				.css("cursor", "pointer")
 				.width("100%")
 				.height("100%")
 				//.css("filter", "alpha(opacity=0)")
 				//.css("-moz-opacity", "0")
 				//.css("opacity", "0")
+				.change(function(e){
+					console.info(this.value);
+				})
 				;
+			
 			var default_os = input.offset();
 			var offset_top = input.height() / 2;
 			var offset_left = (input.width() / 4) * 3;
-			form.mousemove(function(event){
-				input.offset({top: event.clientY - offset_top, left: event.clientX - offset_left});
+			self._form.mousemove(function(e){
+				input.offset({top: e.clientY - offset_top, left: e.clientX - offset_left});
 			}).mouseout(function(){
-				input.offset(default_os);
+				input.offset({top: default_os.top, left: default_os.left});
 			});
+			
+			self._trigger('onInit', null, {button: self.element});
 		});
 	},
 	destroy: function()
 	{
-		$.Widget.prototype.destroy.apply(self, arguments);
-		return self;
+		this._form.remove();
+		$.Widget.prototype.destroy.apply(this, arguments);
+		return this;
 	}
 });
 
