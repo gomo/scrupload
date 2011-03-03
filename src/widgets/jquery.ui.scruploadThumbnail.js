4f86e74cd7b07481b690507f8efcf36c771679d8
Masamoto Miyata <miyata@sincere-co.com>
2011-03-03 16:09:31 +0000

    ダイアログ見た目調整
    git-svn-id: https://scrupload.googlecode.com/svn/branches/dev@59 6a939483-aa53-8207-cb86-4a7756a7ee3a

--- src/widgets/jquery.ui.scruploadThumbnail.js
+++ src/widgets/jquery.ui.scruploadThumbnail.js
@@ -12,7 +12,8 @@ $.widget('ui.scruploadThumbnail', {
 			{
 				var resp = $.parseJSON(ui.response),
 					img,
-					preview;
+					preview,
+					tmp_div;
 				
 				if(resp.error)
 				{
@@ -25,25 +26,31 @@ $.widget('ui.scruploadThumbnail', {
 					return;
 				}
 				
-				//ダイアログdiv
-				self.dialog = $("<div />")
+				var tmp_div = $('<div />')
 					.appendTo(document.body)
-					.append(self.options.dialog_template.html());
+					.css('position', 'absolute')
+					.css('top' ,'-10000px')
+					.css('left', '-10000px');
 				
 				//メイン画像
-				img = $('<img src="'+resp.path+'" />');
-				self.dialog.find(".scr_thumb_image").append(img);
-				
+				img = $('<img src="'+resp.path+'" />').appendTo(tmp_div);
 				//プレビュー
-				preview = $('<img src="'+resp.path+'" />');
-				self.dialog.find(".scr_thumb_preview")
-					.append(preview)
-					.width(self.options.width)
-					.height(self.options.height)
-					.css('overflow', 'hidden');			
+				preview = $('<img src="'+resp.path+'" />').appendTo(tmp_div);
 				
-				
-				img.bind('load', function(){
+				img.load(function(){
+					//ダイアログdiv
+					self.dialog = $("<div />")
+						.appendTo(document.body)
+						.append(self.options.dialog_template.html());
+					
+					self.dialog.find(".scr_thumb_image").append(img);
+					
+					self.dialog.find(".scr_thumb_preview")
+						.width(self.options.width)
+						.height(self.options.height)
+						.css('overflow', 'hidden')
+						.append(preview);
+					
 					self.dialog.dialog($.extend(self.options.dialog, {
 						close:function(event, ui)
 						{
@@ -53,46 +60,60 @@ $.widget('ui.scruploadThumbnail', {
 						}
 					}));
 					
-					var size = {width: img.width(), height: img.height()},
-						disable_selection = false,
-						showPreview
-					;
-					
-					showPreview = function(coords){
-						
-						if(!disable_selection)
-						{
-							self.dialog.find(".jcrop-holder *").disableSelection();
-							disable_selection = true;
-						}
-						
-						self.coords = coords;
-						var rx = size.width / coords.w;
-						var ry = size.height / coords.h;
-						
-						var prew = Math.round(rx * self.options.width);
-						var preh = Math.round(ry * self.options.height);
-						
-						preview.css({
-						    width: prew + 'px',
-						    height: preh + 'px',
-						    marginLeft: '-' + Math.round(coords.x * (prew / size.width)) + 'px',
-							marginTop: '-' + Math.round(coords.y * (preh / size.height)) + 'px'
-						});
-					};
-					
 					self.dialog.find("*").disableSelection();
-					
-					img.Jcrop({
-						onChange: showPreview,
-						onSelect: showPreview,
-						aspectRatio: self.options.width / self.options.height,
-						minSize: [self.options.width, self.options.height],
-						setSelect: [0, 0, self.options.width, self.options.height]
-					});
+					self._initJcrop(img, preview);
 				});
+				
+				/*img.bind('load', function(){
+					
+					
+					
+					
+					
+					
+
+				});*/
 			}
 		}));
+	},
+	_initJcrop: function(img, preview)
+	{
+		var size = {width: img.width(), height: img.height()},
+			disable_selection = false,
+			showPreview,
+			self = this
+		;
+		
+		showPreview = function(coords){
+			
+			if(!disable_selection)
+			{
+				self.dialog.find(".jcrop-holder *").disableSelection();
+				disable_selection = true;
+			}
+			
+			self.coords = coords;
+			var rx = size.width / coords.w;
+			var ry = size.height / coords.h;
+			
+			var prew = Math.round(rx * self.options.width);
+			var preh = Math.round(ry * self.options.height);
+			
+			preview.css({
+			    width: prew + 'px',
+			    height: preh + 'px',
+			    marginLeft: '-' + Math.round(coords.x * (prew / size.width)) + 'px',
+				marginTop: '-' + Math.round(coords.y * (preh / size.height)) + 'px'
+			});
+		};
+		
+		img.Jcrop({
+			onChange: showPreview,
+			onSelect: showPreview,
+			aspectRatio: self.options.width / self.options.height,
+			minSize: [self.options.width, self.options.height],
+			setSelect: [0, 0, self.options.width, self.options.height]
+		});
 	},
 	destroy: function()
 	{
